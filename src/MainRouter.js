d63ab73c4290c5c1f10e69ba7d99f4de62e93892
Aman Kumar Tiwari <kumaramantiwari98@gmail.com>
2020-03-22 04:37:14 +0530

    menu added

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -3,9 +3,13 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./core/Home";
 import Signup from "./user/Signup";
 import Signin from "./user/Signin";
+import Menu from "./core/Menu";
 
 const MainRouter = () => (
   <div>
+ 
+     <Menu/>
+     
     <Switch>
         <Route exact path="/"  component={Home} ></Route>
         <Route exact path="/signup"  component={Signup} ></Route>
